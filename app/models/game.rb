73af2394236202b5ef26c60ebf1a6029396d32fe
fasz <fk1018@users.noreply.github.com>
2025-06-17 12:48:28 -0500

    Add ActionCable game channel and update README

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
   has_many :players, dependent: :destroy
   has_many :items, dependent: :destroy
+  has_many :submissions, through: :players
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :game
   has_many :submissions, dependent: :destroy
+  after_create_commit -> { GameChannel.broadcast_to(game, type: "new_item", item: as_json) }
 end
