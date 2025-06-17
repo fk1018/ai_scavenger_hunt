0e9f06f452f3fd0fbbba4a86dfad5f2666a0ff22
fasz <fk1018@users.noreply.github.com>
2025-06-17 12:39:48 -0500

    Fix ApplicationRecord to inherit from ActiveRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
-# The application was generated without the Active Record framework. When
-# running in continuous integration, Rails will eager load files in
-# `app/models`, which would normally load ActiveRecord. Defining a minimal
-# `ApplicationRecord` prevents `NameError` exceptions when Active Record is not
-# available.
-class ApplicationRecord
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
 end
